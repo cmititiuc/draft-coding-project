952c052bea3761edd0ac2caf514c7b4cdf579e6c
Cat <Catalin.Mititiuc@gmail.com>
2018-03-20 20:32:26 -0400

    Import some players

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+require 'json'
+
+['football', 'baseball', 'basketball'].each { |sport|
+  puts "\nImporting #{sport} players...\n"
+
+  results = Net::HTTP.get(
+    'api.cbssports.com',
+    "/fantasy/players/list?version=3.0&SPORT=#{sport}&response_format=JSON"
+  )
+  players = JSON.parse(results)['body']['players']
+
+  players.take(10).each { |player|
+    Player.create(
+      first_name: player['firstname'],
+      last_name: player['lastname'],
+      position: player['position'],
+      age: player['age'],
+      type: "#{sport.titleize}Player"
+    )
+  }
+
+}
+
+puts "\nDone\n\n"
