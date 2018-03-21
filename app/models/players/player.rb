3aac0e46fb2fc3836c087e1688b2b6834988614a
Cat <Catalin.Mititiuc@gmail.com>
2018-03-21 14:42:08 -0400

    Rename method name for clarity

--- app/models/players/player.rb
+++ app/models/players/player.rb
@@ -1,11 +1,11 @@
 class Player < ApplicationRecord
   def average_position_age_diff
-    (self.age - average_age).round(1) if self.age
+    (self.age - average_age_for_position).round(1) if self.age
   end
 
   private
 
-  def average_age
+  def average_age_for_position
     Rails.cache.fetch("#{self.type}/#{self.position}", expires_in: 1.minute) do
       ages = self.class
         .where(position: self.position)
