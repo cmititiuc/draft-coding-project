c6478fd7554fdb7c084056c6c50478e00b295f01
Cat <Catalin.Mititiuc@gmail.com>
2018-03-21 13:52:31 -0400

    Add caching

--- app/models/players/player.rb
+++ app/models/players/player.rb
@@ -1,10 +1,17 @@
 class Player < ApplicationRecord
   def average_position_age_diff
-    ages = self.class
-      .where(position: self.position)
-      .map { |player| player.age }
-      .compact
-    average_age = ages.inject { |sum, age| sum + age }.to_f / ages.size
     (self.age - average_age).round(1) if self.age
+  end
+
+  private
+
+  def average_age
+    Rails.cache.fetch("#{self.type}/#{self.position}", expires_in: 1.minute) do
+      ages = self.class
+        .where(position: self.position)
+        .map { |player| player.age }
+        .compact
+      ages.inject { |sum, age| sum + age }.to_f / ages.size
+    end
   end
 end
